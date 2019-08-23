3cff5e47e58db5546254f7a4ea841dced0d8267a
Maurck <49370261+Maurck@users.noreply.github.com>
2019-08-23 09:42:30 -0500

    Add files via upload

---
+++ Ejercicios/LeonardoYaVasACaerGAAAAA.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leonardo.ya.vas.a.caer.gaaaaa;
+
+/**
+ *
+ * @author LaboratorioFISI
+ */
+public class LeonardoYaVasACaerGAAAAA {
+   
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int a=1;
+        
+        for(int i=0;i<20;i++)
+        {
+            System.out.println("Alamos manito x" + a);
+            a++;
+        }
+    }
+    
+}
